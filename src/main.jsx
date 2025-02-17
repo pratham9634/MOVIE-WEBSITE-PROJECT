7c0fe05695ea291be1328b3ee03962ffc6eaabdf
pratham9634 <160828246+pratham9634@users.noreply.github.com>
2025-02-17 18:54:40 +0530

    one more changes

--- src/main.jsx
+++ src/main.jsx
@@ -2,15 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { SearchProvider } from './contexts/Searchcontext'
+import { SearchProvider } from './contexts/SearchContext'
 import { BrowserRouter } from 'react-router'
 
 createRoot(document.getElementById('root')).render(
   <SearchProvider>
     <BrowserRouter>
-  <StrictMode>
-    <App />
-  </StrictMode>
-  </BrowserRouter>
+      <StrictMode>
+        <App />
+      </StrictMode>
+    </BrowserRouter>
   </SearchProvider>
 );  
